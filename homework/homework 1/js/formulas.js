a82fcf290635a7b852c1ebbd24af67dddcb35727
hopper <ASO@hoppers-macbook.attlocal.net>
2016-09-22 01:52:16 -0700

    inital homework 1 code

---
+++ homework/homework 1/js/formulas.js
@@ -0,0 +1,35 @@
+
+// Instructions for your homework
+// //////////////////////////////////////////////////////////////////
+// 1.   Here is where your functions should be defined
+// 2.	 What should you name your functions?  Hint:  check the console to see which functions are already being called.  Are they all "defined?"  If not yet defined... then define them here!
+// 3.	 Be sure to link up this file in your HTML doc
+/////////////////////////////////////////////////////////////////////
+
+// Fahrenheit to celcius: (fahrenheit - 32) * 5/9
+function calcFahrenheitToCelcius(tempF) {
+        return (tempF - 32) * 5/9
+}
+
+// Celcius to fahrenheit: celcius * (9/5) + 32
+function calcCelciusToFarenheit(tempC) {
+	return tempC * (9/5) + 32	
+}
+
+// Radius to circumference: 2 * Math.PI * radius
+function calcCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+// Pythagorean theorem: Math.sqrt((a * a) + (b * b))
+// Not finished
+
+function calcLongestSide (a,b) {
+	return Math.sqrt((a * a) + (b * b));
+}
+
+
+
+
+
+
